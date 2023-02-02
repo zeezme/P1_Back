58d89fbf0e8aeb04d6a2941742d2f675eb5d7619
menelucas <115168314+menelucas@users.noreply.github.com>
2023-02-02 17:37:58 -0300

    Removed Console Log

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -5,7 +5,6 @@ const User = db.user
 
 verifyToken = (req, res, next) => {
   let token = req.headers["x-access-token"]
-  console.log(req.body)
   if (!token) {
     return res.status(403).send({
       message: "No token provided!",
