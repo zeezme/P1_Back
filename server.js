d7d7a92a7517b5090b22dcddcb6c003d2580b992
menelucas <115168314+menelucas@users.noreply.github.com>
2023-04-25 11:44:05 -0300

    Correções Gerais

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // database
-const db = require("./app/models")
+// force: true will drop the table if it already exists
+
+/* const db = require("./app/models")
 const Role = db.role
 
 db.sequelize.sync()
 
-// force: true will drop the table if it already exists
-
-/* db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync Database with { force: true }")
   initial()
 }) */
@@ -46,20 +46,3 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user",
-  })
-
-  Role.create({
-    id: 2,
-    name: "moderator",
-  })
-
-  Role.create({
-    id: 3,
-    name: "admin",
-  })
-}
