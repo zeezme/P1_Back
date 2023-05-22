08caa3a344dc5143a723084fc2883e06bb626dc9
zeezme <lukascass@gmail.com>
2023-05-21 22:56:55 -0300

    Postgres

--- app/models/index.js
+++ app/models/index.js
@@ -1,30 +1,7 @@
 const Sequelize = require("sequelize")
-/* //Postgres
+//Postgres
 const sequelize = new Sequelize(
   `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_ADDRESS}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-) */
-
-//MSSQL
-/* const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_ADDRESS,
-    dialect: "mssql",
-  }
-) */
-
-//MYSQL
-const { development } = require("./app/config/config.json")
-const sequelize = new Sequelize(
-  development.database,
-  development.username,
-  development.password,
-  {
-    host: development.host,
-    dialect: development.dialect,
-  }
 )
 
 module.exports = sequelize
