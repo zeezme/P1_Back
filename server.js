6b281187162b8404f62faf4efcd2e44ea3f1b1bb
zeezme <lukascass@gmail.com>
2023-06-13 20:32:22 -0300

    server.js simplified

--- server.js
+++ server.js
@@ -1,52 +1,15 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-/* const morgan = require("morgan") */
 require("dotenv").config()
 
+const express = require("express")
+const cors = require("cors")
+
 const app = express()
-console.log("Informações do front connectado:")
-console.log(`http://${process.env.CLIENT_ADDRESS}:${process.env.CLIENT_PORT}`)
-var corsOptions = {
-  origin: `*`,
-}
+const router = require("./app/routes/router")
+const bodyParser = require("body-parser")
 
-app.use(cors(corsOptions))
-
-/* app.use(morgan("combined")) */
-
-// parse requests of content-type - application/json
+app.use(cors())
 app.use(bodyParser.json())
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use("/", router)
 
-// database
-// force: true will drop the table if it already exists
-
-/* const db = require("./app/models")
-const Role = db.role
-
-db.sequelize.sync()
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and Resync Database with { force: true }")
-  initial()
-}) */
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Running." })
-})
-
-// routes
-require("./app/routes/auth.routes")(app)
-require("./app/routes/user.routes")(app)
-require("./app/routes/shopify.routes")(app)
-require("./app/routes/patient.routes")(app)
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`)
-})
+app.listen(8080)
